2c1e420fa9b14accf2e8a317f3fd3ad321c5ac98
Tatsuki Hayashi <hayashideveloper@gmail.com>
2019-10-19 22:59:49 +0900

    Rails 5.2.3へversion up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,30 @@
 class PostsController < ApplicationController
     def index
+        @posts = Post.all
+    end
 
+    def new
+        @post = Post.new
+    end
+
+    def create 
+        post = Post.new(post_params)
+        
+        if post.save 
+            flash[:success] = "投稿しました。"
+            redirect_to posts_path
+        else
+            redirect_to new_post_path
+        end
+    end
+
+    def destroy
+        @post.destroy
+        redirect_to posts_path, flash: { notice: "削除しました。" }
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:body, :user_id)
     end
 end
---
+++ db/migrate/20191018123746_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :body
+      t.integer :user_id
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
