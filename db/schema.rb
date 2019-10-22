82a1abf63c85455bf4ae935087ea0b23e11c27f5
Tatsuki Hayashi <hayashideveloper@gmail.com>
2019-10-22 18:31:58 +0900

    コメント画面でBootstrapを使用できるように修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_145124) do
+ActiveRecord::Schema.define(version: 2019_10_22_062724) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2019_10_20_145124) do
     t.string "body"
     t.integer "user_id"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "sort"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
