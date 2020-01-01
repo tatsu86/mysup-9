305edcba631962436da9385401b4648f98324295
Tatsuki Hayashi <hayashideveloper@gmail.com>
2020-01-02 03:41:18 +0900

    投稿日時の表示変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,34 @@ module ApplicationHelper
         content_tag :li, class: class_name do
           link_to name, path, class: 'nav-link'
         end
+    end
+
+    def GetDateFormat(date)
+      now = DateTime.current
+      today = DateTime.current.to_date
+      if today.year == date.year
+        if (today - date.to_date).to_i <= 7
+          if today == date.to_date
+            if now.hour == date.hour
+              # 1時間以内
+              result = (now.min - date.min).to_s + "分前"
+            else
+              # 当日
+              result = (now.hour - date.hour).to_s + "時間前"
+            end
+          else
+            # 一週間以内
+            result = (today - date.to_date).to_i.to_s + "日前"
+          end
+        else
+          # 今年
+          result = date.strftime("%m月%d日")
+        end
+      else
+        # 去年以前
+        result = date.strftime("%Y年%m月%d日")
       end
+      
+      return result
+    end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,2 @@
-Time::DATE_FORMATS[:datetime_jp] = '%Y/%m/%d %H:%M'
+Time::DATE_FORMATS[:datetime_jp] = '%Y年%m月%d日'
+Time::DATE_FORMATS[:datetime_jp_this_year] = '%m月%d日'
