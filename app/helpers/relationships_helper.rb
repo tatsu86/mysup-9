1d399d36c6be9f1db203ce19cb651731ea1a229a
Tatsuki Hayashi <hayashideveloper@gmail.com>
2020-02-29 15:53:08 +0900

    ユーザーのステータス表示

--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -1,2 +1,6 @@
 module RelationshipsHelper
+  # フォロワー数を取得する
+  def follower_count(user_id)
+    return Relationship.where(follow_id: user_id).count
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,9 @@ module UsersHelper
     end
     return (Time.zone.today.strftime("%Y%m%d").to_i - birthday.gsub!("-","").to_i) / 10000
   end
+
+  # 特定ユーザーの投稿数を取得する
+  def user_posts_count(user_id)
+    return Post.where(user_id: user_id).count
+  end
 end
