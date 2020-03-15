feed2ec4bbe394f0afc2ab8d5ec2387b0f7637b4
Tatsuki Hayashi <hayashideveloper@gmail.com>
2020-03-15 10:53:09 +0900

    画像のプレビュー機能実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,23 @@
 //= require turbolinks
 //= require_tree .
 
+// フラッシュをフェードアウトする
 $(function(){
   setTimeout("$('.flash').fadeOut('slow')",2000);
+});
+
+// 画像のプレビュー
+$(function() {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $("#img_prev").attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#file").change(function(){
+    readURL(this);
+  });
 });
